b274ecba2f1790164189e8120999ef5f66fbe9cf
Kevin Su <44729913+KSu2@users.noreply.github.com>
2023-06-05 17:22:13 -0400

    chore: start inventory functionality

--- Assets/Scripts/Menus/inventoryMenu.cs
+++ Assets/Scripts/Menus/inventoryMenu.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class inventoryMenu : MonoBehaviour
 {
     public GameObject invMenuObj;
+    [SerializeField] public GameObject slot1;
+    [SerializeField] public GameObject slot2;
+    [SerializeField] public GameObject slot3;
+
     public static bool invOpen;
 
     // Start is called before the first frame update
@@ -16,34 +21,56 @@ public class inventoryMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!pauseMenu.isPaused)
+        if (!pauseMenu.isPaused)
         {
-            
+
         }
-            if(Input.GetButtonDown("Inv"))
+        if (Input.GetButtonDown("Inv"))
+        {
+            if (invOpen)
             {
-                if(invOpen)
-                {
-                    CloseInv();
-                }
-                else
-                {
-                    OpenInv();
-                }
+                CloseInv();
+            }
+            else
+            {
+                OpenInv();
             }
         }
+    }
 
-        public void OpenInv()
+    public void OpenInv()
+    {
+        invMenuObj.SetActive(true);
+        invOpen = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void CloseInv()
+    {
+        invMenuObj.SetActive(false);
+        invOpen = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void updateText(string text, int invSlot)
+    {
+        if (invSlot == 0)
         {
-            invMenuObj.SetActive(true);
-            invOpen = true;
-            Cursor.lockState = CursorLockMode.None;
+            Debug.Log("text: " + slot1.GetComponent<TextMeshProUGUI>().text);
+            slot1.GetComponent<TextMeshProUGUI>().text = text;
+        }
+        else if (invSlot == 1)
+        {
+            slot2.GetComponent<TextMeshProUGUI>().text = text;
+        }
+        else if (invSlot == 2)
+        {
+            slot3.GetComponent<TextMeshProUGUI>().text = text;
+        }
+        else
+        {
+            Debug.Log("ERROR: trying to access an inv slot number that doesn't exist");
         }
 
-        public void CloseInv()
-        {
-            invMenuObj.SetActive(false);
-            invOpen = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
+    }
 }
