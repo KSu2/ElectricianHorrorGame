6b19235bb0a440fac731972282ec503133630c73
Kevin Su <44729913+KSu2@users.noreply.github.com>
2023-06-04 22:30:42 -0400

    feat: adding item to inventory

---
+++ Assets/Scripts/ItemActivate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemActivate : MonoBehaviour, IInteractable
+{
+    //determine the item type
+    public enum Type { None = 0, health };
+    [SerializeField] private Type select;
+    public GameObject inv;
+    public int invCount = 0;
+
+    public void Interact()
+    {
+        gameObject.SetActive(false);
+        //the inventory slots should fill up sequentially, so first 1 then 2 then 3
+        //first check if the inventory is full
+        if(invCount == 2)
+        {
+            Debug.Log("inventory is full!");
+        } 
+        else if(select == Type.health)
+        {
+            Debug.Log("selected health item");
+            inv.GetComponent<inventoryMenu>().updateText("health", invCount);
+            invCount++;
+        }
+    }
+}
--- Assets/Scripts/Menus/inventoryMenu.cs
+++ Assets/Scripts/Menus/inventoryMenu.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class inventoryMenu : MonoBehaviour
 {
     public GameObject invMenuObj;
+    [SerializeField] public GameObject slot1;
+    [SerializeField] public GameObject slot2;
+    [SerializeField] public GameObject slot3;
+
     public static bool invOpen;
 
     // Start is called before the first frame update
@@ -31,19 +36,41 @@ public class inventoryMenu : MonoBehaviour
                     OpenInv();
                 }
             }
-        }
+    }
 
-        public void OpenInv()
-        {
+    public void OpenInv()
+    {
             invMenuObj.SetActive(true);
             invOpen = true;
             Cursor.lockState = CursorLockMode.None;
-        }
+    }
 
-        public void CloseInv()
-        {
+    public void CloseInv()
+    {
             invMenuObj.SetActive(false);
             invOpen = false;
             Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void updateText(string text, int invSlot)
+    {
+        if(invSlot == 0)
+        {
+            Debug.Log("text: " + slot1.GetComponent<TextMeshProUGUI>().text);
+            slot1.GetComponent<TextMeshProUGUI>().text = text;
+        } 
+        else if(invSlot == 1)
+        {
+            slot2.GetComponent<TextMeshProUGUI>().text = text;
         }
+        else if(invSlot == 2)
+        {
+            slot3.GetComponent<TextMeshProUGUI>().text = text;
+        }
+        else
+        {
+            Debug.Log("ERROR: trying to access an inv slot number that doesn't exist");
+        }
+
+    }
 }
